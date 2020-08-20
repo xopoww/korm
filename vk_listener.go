10a9db629b1ad896f2722ef745fd5844b821f92a
Arseny Khoroshilov <horoshilov.aa@phystech.edu>
2020-08-20 20:23:53 +0300

    hello world

--- vk_listener.go
+++ vk_listener.go
@@ -9,5 +9,5 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello World!")
 	})
-	http.ListenAndServe(":80", nil)
+	http.ListenAndServe(":8888", nil)
 }
